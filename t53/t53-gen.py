ba34df94dd5973f1b99dba2895156ff36b9f08d8
christos k <ckatsak@gmail.com>
2016-11-12 07:04:34 +0200

    t53 now has its benchmark and hopefully still works too

--- t53/t53-gen.py
+++ t53/t53-gen.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
-
+#
+# t53-gen.py
+#
+# NOTE: Each time this script is run, ./t53-timed.h gets overwritten!
+#
+# Create a C header file for t53-timed.c, containing an array of unsigned long
+# long int, named rands, to be used for benchmarking.
+#
+# Author: ckatsak
+#
 import random
 
-SIZE = 1000000
+header_file = "t53-timed.h"
+array_size  = 10000
+range64_min = 1 << 63
+range64_max = (1 << 64) - 1
 
 def main():
-    """
-    NOTE: Each time this script is run, ./t53.h gets overwritten!
-
-    Create a C header file for t53.c, containing an array of long long int,
-    named rands, to perform an accurate benchmark.
-    """
-
-    with open("t53.h", 'w') as fout:
-        print("#ifndef __T53_H__", file=fout)
-        print("#define __T53_H__", file=fout)
-        print("\n#define SIZE\t{}".format(SIZE), file=fout)
-        print("\nextern long long rands[] = {", file=fout)
-        for i in range(SIZE // 4):
-            print("\t{},\t{},\t{},\t{},".format(random.getrandbits(64),
-                random.getrandbits(64), random.getrandbits(64),
-                random.getrandbits(64)), file=fout)
-        print("}\n\n#endif /* __T53_H__ */", file=fout)
+    with open(header_file, 'w') as fout:
+        print("#ifndef __T53_H__\n#define __T53_H__", file=fout)
+        print("\n#define ARRAY_SIZE\t{}".format(array_size), file=fout)
+        print("\n#define RANGE64_MIN\t{}ULL\t/* 1<<63 */".format(range64_min), file=fout)
+        print("#define RANGE64_MAX\t{}ULL\t/* 1<<64 - 1*/".format(range64_max), file=fout)
+        print("\n// Ignore the useless warning, more info at " +
+			"https://gcc.gnu.org/bugzilla/show_bug.cgi?id=45977".format(), file=fout)
+        print("extern unsigned long long rands[] = {", file=fout)
+        for i in range(array_size // 4):
+            print("\t" + " {}ULL,".format(random.randint(range64_min, range64_max)) * 4, file=fout)
+#            print("\t" + " {}ULL,".format(random.getrandbits(64)) * 4, file=fout)
+        print("};\n\n#endif /* __T53_H__ */", file=fout)
 
 if __name__ == "__main__":
     main()
---
+++ t53/t53-timed.c
@@ -0,0 +1,66 @@
+/*
+ * Testing likely(x) and unlikely(x), GCC extensions macros.
+ *
+ * Build with and without the macros
+ * $ gcc t53-timed.c -Wall -o t53-timed-with -O0
+ * $ gcc t53-timed.c -Wall -o t53-timed-without -O0 -DWITHOUT
+ * (Maybe also use flag -fno-guess-branch-probability ?)
+ *
+ * Now time both versions
+ * $ time ./t53-timed-with
+ * $ time ./t53-timed-without
+ *
+ * Build with and without the macros into assembly code, and inspect it, spot
+ * the magic, if any
+ * $ gcc t53-timed.c -Wall -S -o t53-timed-with.S -O0
+ * $ gcc t53-timed.c -Wall -S -o t53-timed-without.S -O0 -DWITHOUT
+ * $ diff -y t53-timed-with{.S,out.S} | less
+ *
+ * Author: ckatsak
+ *
+ * TODO: Benchmark with a proper random number generator function.
+ * TODO: Makefile
+ */
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/time.h>
+
+#include "t53-timed.h"
+
+#ifndef WITHOUT
+ #define likely(x)	__builtin_expect (!!(x), 1)
+ #define unlikely(x)	__builtin_expect (!!(x), 0)
+#else
+ #define likely(x)	(x)
+ #define unlikely(x)	(x)
+#endif
+
+int main(void)
+{
+	register unsigned long long i, j;
+	struct timeval start, end;
+
+	if (gettimeofday(&start, NULL)) {
+		perror("gettimeofday(start)");
+		return EXIT_FAILURE;
+	}
+
+	for (j = 0; j < ARRAY_SIZE; j++) 
+		for (i = RANGE64_MIN; i < RANGE64_MAX - 1; i++)
+			if (unlikely(i == rands[j]))
+				break;
+
+	if (gettimeofday(&end, NULL)) {
+		perror("gettimeofday(end)");
+		return EXIT_FAILURE;
+	}
+
+	fprintf(stderr, "\nTime elapsed: %lldusec.\n\n",
+			(end.tv_sec - start.tv_sec) * 1000000 +
+			(end.tv_usec - start.tv_usec));
+
+	return EXIT_SUCCESS;
+}
