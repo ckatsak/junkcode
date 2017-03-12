ad7ffb3022e6a4543e40c0e8484256195a66c27d
christos k <ckatsak@gmail.com>
2017-03-12 06:24:29 +0200

    Go: String/Byte manipulation and Env vars.

---
+++ t74.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func init() {
+	os.Setenv("ENV_VAR", "web-42")
+}
+
+func main() {
+	env := os.Getenv("ENV_VAR")
+
+	var id int
+	if i := bytes.LastIndexByte([]byte(env), []byte("-")[0]); i != -1 {
+		var err error
+		if id, err = strconv.Atoi(env[i+1:]); err != nil {
+			fmt.Println("Atoi:", err)
+		}
+	}
+
+	fmt.Println("env:", env)
+	fmt.Println("id:", id)
+
+	return
+}
