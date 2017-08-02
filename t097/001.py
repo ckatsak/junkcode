9440db45c21448113a818affe40e77fee2043355
christos k <ckatsak@gmail.com>
2017-08-02 19:37:36 +0300

    test K8s watch API: deployment objects

---
+++ t097/001.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from pprint import pprint
+
+from kubernetes import (
+    client as klient,
+    config as konfig,
+    watch as kwatch,
+)
+
+
+def main():
+    konfig.load_incluster_config()
+    v1b1 = klient.AppsV1beta1Api()
+
+    w = kwatch.Watch()
+    try:
+        for e in w.stream(v1b1.read_namespaced_deployment,
+                          'pynsider',
+                          os.getenv('POD_NAMESPACE'),
+                          ):
+            pprint(e)
+    except klient.rest.ApiException as err:
+        print err
+
+
+if __name__ == '__main__':
+    main()
