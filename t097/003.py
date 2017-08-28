6fe7a2b4dfcdc735c1cc1519c5a8d57d463f8962
christos k <ckatsak@gmail.com>
2017-08-28 16:26:27 +0300

    More k8s watch api

---
+++ t097/003.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+from pprint import pprint
+
+from kubernetes import (
+    client as klient,
+    config as konfig,
+    watch as kwatch,
+)
+
+
+def main():
+    konfig.load_incluster_config()
+    v1b1 = klient.AppsV1beta1Api()
+
+    w = kwatch.Watch()
+    try:
+        for e in w.stream(v1b1.list_namespaced_deployment,
+                          os.getenv('POD_NAMESPACE'),
+                          label_selector='app=pyntainerdb',
+                          timeout_seconds=180,
+                          watch=True,
+                          ):
+            typ = e['type']
+            obj = e['object']
+            raw = e['raw_object']
+
+            pprint(obj)
+            print type(obj)
+            if isinstance(obj,
+                klient.models.apps_v1beta1_deployment.AppsV1beta1Deployment):
+                print 'WOOHOO'
+    except klient.rest.ApiException as err:
+        print err
+
+
+if __name__ == '__main__':
+    main()
