544c7f2194de6299a111389fb108d5cf2f5bd0d2
christos k <ckatsak@gmail.com>
2017-09-19 05:35:35 +0300

    testing named pipes + python

---
+++ t106/a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import logging
+import os
+
+import comm
+
+
+def compute():
+    for i in range(comm.COMPUTE_SECS, 0, -1):
+        logging.info("Computing: %d...", i)
+        comm.sleep()
+
+
+def main():
+    comm.mkfifoz()
+
+    logging.info("Opening '%s' for writing...", comm.FIFO1)
+    fifo = open(comm.FIFO1, 'w')
+    # fifo = open(comm.FIFO1, 'w', comm.UNBUFFERED)
+    # fifo = open(comm.FIFO1, 'w', comm.LINE_BUFFERED)
+    logging.info("Just opened '%s' for writing!", comm.FIFO1)
+
+    for i in range(comm.COMPUTE_TIMES):
+        compute()
+        logging.info("Writing '%s' to '%s'...", str(i), comm.FIFO1)
+        fifo.write('%s\n' % i)
+        fifo.flush()
+        logging.info("Just wrote to '%s'!", comm.FIFO1)
+    os._exit(1)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel('DEBUG')
+    main()
---
+++ t106/b.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import logging
+
+import comm
+
+
+def job(num):
+    for i in xrange(comm.JOB_SECS, 0, -1):
+        logging.info("Job '%s': %d...", num, i)
+        comm.sleep()
+
+
+def main():
+    comm.mkfifoz()
+
+    logging.info("Opening '%s' for reading...", comm.FIFO1)
+    fifo = open(comm.FIFO1, 'r')
+    # fifo = open(comm.FIFO1, 'r', comm.UNBUFFERED)
+    # fifo = open(comm.FIFO1, 'r', comm.LINE_BUFFERED)
+    logging.info("Just opened '%s' for reading!", comm.FIFO1)
+
+    while True:
+        logging.info("Reading from '%s'...", comm.FIFO1)
+        x = fifo.readline().strip()
+        logging.info("Just read '%s' from '%s'...", x, comm.FIFO1)
+        if x is None or x == '':  # EOF
+            break
+        job(x)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel('DEBUG')
+    main()
