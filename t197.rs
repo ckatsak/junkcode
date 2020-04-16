f05093581a97906974cc7cbca0afd95622298666
Christos Katsakioris <ckatsak@gmail.com>
2020-04-16 23:22:28 +0300

    t197: [Rust] Test std::path::PathBuf.extension
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

---
+++ t197.rs
@@ -0,0 +1,22 @@
+//! Test std::path::PathBuf.extension
+//!
+//! $ rustc t197.rs
+//! $ ./t197
+
+use std::path::PathBuf;
+
+fn main() {
+    let dez = vec![
+        PathBuf::from("A.a"),
+        PathBuf::from("B.b.b"),
+        PathBuf::from("C"),
+        PathBuf::from("D_d"),
+        PathBuf::from(".E"),
+        PathBuf::from(".F.f_f"),
+        PathBuf::from("G.g_g"),
+    ];
+
+    for e in &dez {
+        eprintln!("{:?}\t--> {:?}", e, e.extension());
+    }
+}
