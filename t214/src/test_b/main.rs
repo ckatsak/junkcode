ba27169d705cda2249079cd60f1642c509e3f6d2
Christos Katsakioris <ckatsak@gmail.com>
2023-01-11 20:50:20 +0200

    t214: [Rust] tokio semaphores
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

---
+++ t214/src/test_b/main.rs
@@ -0,0 +1,50 @@
+use std::{sync::Arc, time::Duration};
+
+use anyhow::{anyhow, Context, Result};
+use tokio::{sync::Semaphore, time::sleep};
+use tracing::{debug, info};
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    ::tracing_subscriber::fmt()
+        .with_test_writer()
+        .with_env_filter(
+            EnvFilter::from_default_env().add_directive(
+                "test_b=trace"
+                    .parse()
+                    .with_context(|| "failed to parse filtering directive")?,
+            ),
+        )
+        .with_thread_ids(true)
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .try_init()
+        .map_err(|err| anyhow!("failed to initialize tracing subscriber: {err}"))?;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    let semaphore = Arc::new(Semaphore::new(3));
+    let mut join_handles = Vec::new();
+
+    for i in 0..10 {
+        info!("[main] iteration {i}");
+        let permit = semaphore.clone();
+        join_handles.push(tokio::spawn(async move {
+            let _permit = permit
+                .acquire()
+                .await
+                .expect("failed to acquire the semaphore");
+            // perform task...
+            debug!("[task {i}]");
+            sleep(Duration::from_secs(2)).await;
+            //
+            //drop(permit);
+        }));
+    }
+
+    for handle in join_handles {
+        handle.await.unwrap();
+    }
+
+    Ok(())
+}
